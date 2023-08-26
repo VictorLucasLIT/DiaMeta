cb89e4d4de392c2515d865e8a010b95f5c3a3737
VictorLucasLIT <133718091+VictorLucasLIT@users.noreply.github.com>
2023-08-26 18:06:11 -0300

    Implementação do banco de dados(com bugs para resolver)

---
+++ App/Assets/scripts/Cadastro.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mono.Data.Sqlite;
+using Mono.Data;
+using System.Data;
+
+public class Cadastro : MonoBehaviour
+{
+    public string DataBaseNome;
+    public InputField NomeInput;
+    public InputField SobrenomeInput;
+    public InputField EmailInput;
+    public InputField SenhaInput;
+
+    public void InserirInfo()
+    {
+        var _NomeInput = NomeInput.text.Trim();
+        var _SobrenomeInput = SobrenomeInput.text.Trim();
+        var _EmailInput = EmailInput.text.Trim();
+        var _SenhaInput = SenhaInput.text.Trim();
+
+        string conn = SetDataBaseClass.SetDataBase(DataBaseNome + ".db");
+        IDbConnection dbcon;
+        IDbCommand dbcmd;
+        IDataReader reader;
+
+        dbcon = new SqliteConnection(conn);
+        dbcon.Open();
+        dbcmd = dbcon.CreateCommand();
+        string SQlQuery = "Insert Into Usuarios(Nome, Sobrenome, Email, Senha)" +
+                          "Values('" + _NomeInput + "','" + _SobrenomeInput + "','" + _EmailInput + "','" + _SenhaInput + "')";
+        dbcmd.CommandText = SQlQuery;
+        reader = dbcmd.ExecuteReader();
+        while(reader.Read())
+        {
+            //Essa parte e para pegar itens do banco de dados
+        }
+
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbcon.Close();
+        dbcon = null;
+    
+        PlayerPrefs.SetString("Email", "olá");
+    }
+}
